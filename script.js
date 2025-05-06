8a51cd40959a622ae5ceb41b9781ee306f0cc0de
Marta Martin <172967869+martamllop@users.noreply.github.com>
2025-05-06 12:54:45 +0200

    Control d'errors per camps buits al formulari

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ contactes.forEach(nom => {
 	llista.appendChild(li);
  });
 document.body.appendChild(llista);
+
+document.querySelector("form").addEventListener("submit", e => {
+	e.preventDefault();
+	const input = document.querySelector('#nom');
+	if (input.value.trim() === ""){
+		alert("El camp no pot estar buit");
+		return;
+	}
+	const li = document.createElement('li');
+	li.textCOntent = input.value;
+	document.querySelector("ul").appendCHIld(li);
+	input.value = "";
+	});
