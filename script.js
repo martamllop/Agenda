2899c536880418e9832f3f2515970cd68f55ce63
Marta Martin <172967869+martamllop@users.noreply.github.com>
2025-05-06 12:38:59 +0200

    Afegida funcionalitat per mostrar contactes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+
 console.log("Agenda Web"); 
+
+const contactes = ["Jose", "Saida", "Ruben"];
+const llista = document.createElement('ul');
+contactes.forEach(nom => {
+	const li = document.createELement('li');
+	li.textContent = nom;
+	llista.appendChild(li);
+ });
+document.body.appendChild(llista);
